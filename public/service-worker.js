466dc01e0bcf88e1697d9c88f0f8c547f1c6b28b
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-07-20 18:39:59 -0500

    final push

---
+++ public/service-worker.js
@@ -0,0 +1,67 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/index.html",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+ console.log("hello there")
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    console.log("hey whats up")
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache Data", key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    )
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                    .catch(err => {
+                        return cache.match(event.request);
+
+                    });
+            })
+                .catch(err => console.log(err))
+        );
+
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
